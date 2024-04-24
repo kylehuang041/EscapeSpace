1e2585e7e9c7cef61daad06fea178ee250fbda7a
Mute <legendgm@outlook.com>
2024-04-24 11:53:24 -0700

    Stealth Meter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,11 @@ public class PlayerMovement : MonoBehaviour
     private float yawX = 0.0f;
     private bool cursorLocked = true;
 
+    // Crouching
+    public float crouchHeight = 0.5f;
+    private float defaultHeight = 2.0f;
+    private float crouchSpeed = 2.5f;
+
     void Start()
     {
         // Lock cursor to center of screen and hide it
@@ -33,15 +38,21 @@ public class PlayerMovement : MonoBehaviour
             horizontalInput = Input.GetAxis("Horizontal");
             forwardInput = Input.GetAxis("Vertical");
 
+            // Adjust speed
+            float currentSpeed = Input.GetKey(KeyCode.LeftControl) ? crouchSpeed : speed;
+
             // Move the player forward/backward
-            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed * forwardInput);
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
             // Rotate the player based on mouse input for yaw (left/right)
             yawX += sensitivity * Input.GetAxis("Mouse X");
             transform.eulerAngles = new Vector3(0.0f, yawX, 0.0f);
 
             // Rotate left/right of player
-            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+            float targetHeight = Input.GetKey(KeyCode.LeftControl) ? defaultHeight - crouchHeight : defaultHeight;
+            Vector3 currentPosition = transform.localPosition;
+            transform.localPosition = new Vector3(currentPosition.x, targetHeight, currentPosition.z);
         }
         else
         {
