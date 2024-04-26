1283e844b5b988592732e211e1eb9e6ed5a67fa1
Brody <bsoedel@uw.edu>
2024-04-25 19:34:16 -0700

    Updated inspect function to make it scaleable

--- Assets/Scripts/InspectAction.cs
+++ Assets/Scripts/InspectAction.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
 
+// Brody Soedel
+// Script that detects if object that inspect key is pressed on is inspectable or not
+// and launches seperate script if that is the case
+
+// TODO: Pause game while inspecting, and not allowing interaction with the worldspace
 public class InspectAction : MonoBehaviour
 {
-    private Camera _camera; 
-
+    private Camera _camera;
+    public Camera inspectCamera;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        _camera = GetComponent<Camera>();
+        _camera = GetComponent<Camera>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // launches ray from mouseposition, which upon hitting detectable object launches inspect
+        // camera
         if (Input.GetKeyDown(KeyCode.F))
         {
             RaycastHit RayHit;
@@ -26,13 +36,33 @@ public class InspectAction : MonoBehaviour
             {
                 if (RayHit.transform.gameObject.tag == "Inspectable")
                 {
-                    InspectObject.inspect.Invoke();
-                    
+                    // instantiate object under the scene with directional light
+                    // draw camera over current camera
+                    GameObject inspectObject = RayHit.transform.gameObject;
+                    GameObject inspectClone = Instantiate(inspectObject, new Vector3(1000, 1000, 1000), 
+                        Quaternion.identity);
+                    Rigidbody physics = inspectClone.GetComponent<Rigidbody>();
+                    if (physics != null)
+                    {
+                        Destroy(physics);
+                    }
+                    inspectClone.tag = "Untagged";
+                    Vector3 size = inspectObject.GetComponent<Renderer>().bounds.size;
+                    float cameraDistance = Mathf.Max(size.x, size.y, size.z);
+                    // centers object and moves camera far enough back that object doesn't clip through camera
+                    Camera inspectCam = Instantiate(inspectCamera, new Vector3(1000, 1000 + size.y / 2, 
+                        1000 - cameraDistance), Quaternion.identity);
+                    InspectCamera script = inspectCam.GetComponent<InspectCamera>();
+                    script.SetObject(inspectClone);
                 }
             }
         }
     }
 
+    // mousePosition: position of Mouse
+    // RayHit: RaycastHit object which contains information about what it hits, 
+    // passed in as reference
+    // returns boolean that is true if raycast hits object
     private bool CameraToMouseRay(Vector2 mousePosition, out RaycastHit RayHit)
     {
         Ray Ray = _camera.ScreenPointToRay(mousePosition);
--- Assets/Scripts/InspectCamera.cs
+++ Assets/Scripts/InspectCamera.cs
@@ -2,32 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Brody Soedel 
+// Inspect Camera that controls rotation and views object that is beeing inspected
+
+// TODO: Rotation is bad for some objects that have bad transform axes, must develop better 
+// solution, camera also clips into some objects but not others
 public class InspectCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     public float deltaRotationX;
     public float deltaRotationY;
 
-    private Transform _inspectObjectTransform;
-
+    private GameObject _inspectObj;
+    private Transform _inspectObjTransform;
     public int rotateSpeed;
     void Start()
     {
-        _inspectObjectTransform = InspectObject.inspectObject?.Invoke();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // switches back to player view
+        if (Input.GetKeyDown(KeyCode.F)) {
+            Destroy(_inspectObj);
+            Destroy(gameObject);
+        }
         deltaRotationX = -Input.GetAxis("Mouse X");
         deltaRotationY = Input.GetAxis("Mouse Y");
-
-        if (Input.GetMouseButton(1) && _inspectObjectTransform)
+        
+        // spins object based on axis of rotation and change in rotation along camera axes
+        if (Input.GetMouseButton(1) && _inspectObjTransform)
         {
-            _inspectObjectTransform.rotation =
-                Quaternion.AngleAxis(deltaRotationX * rotateSpeed, transform.up) *
-                Quaternion.AngleAxis(deltaRotationY * rotateSpeed, transform.right) *
-                _inspectObjectTransform.rotation;
+            _inspectObjTransform.rotation =
+                Quaternion.AngleAxis(deltaRotationX * rotateSpeed, _inspectObjTransform.up) *
+                Quaternion.AngleAxis(deltaRotationY * rotateSpeed, _inspectObjTransform.right) *
+                _inspectObjTransform.rotation;
         }
+    }
+
+    // setter fucntion that accepts an object as a parameter which is the object that is going
+    // to be inspected. This is passed from InspectAction script usually
+    public bool SetObject(GameObject obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        _inspectObj = obj;
+        _inspectObjTransform = obj.transform;
+        return true;
     }
 }
